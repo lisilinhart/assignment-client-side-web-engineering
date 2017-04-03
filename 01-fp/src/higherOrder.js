8c864269769772604f0c48534d4b57eab1e3aee9
Lisi Linhart <e.w.l@live.at>
2017-04-03 12:53:12 +0200

    add higher order functions

---
+++ 01-fp/src/higherOrder.js
@@ -0,0 +1,29 @@
+export function map(data, fn) {
+  const mappedData = [];
+
+  data.forEach((el) => {
+    mappedData.push(fn(el));
+  });
+
+  return mappedData;
+}
+
+export function filter(data, fn) {
+  const filteredData = [];
+
+  data.forEach((el) => {
+    if (fn(el)) filteredData.push(el);
+  });
+
+  return filteredData;
+}
+
+export function reduce(data, fn) {
+  let reducedData = 0;
+
+  data.forEach((el) => {
+    reducedData = fn(reducedData, fn(el));
+  });
+
+  return reducedData;
+}
